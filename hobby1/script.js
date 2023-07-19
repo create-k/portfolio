fa27e5ffac99af0c16c20aeeb3b33a86847c8276
create-k <131327436+create-k@users.noreply.github.com>
2023-07-20 08:53:51 +0900

    Add files via upload

---
+++ hobby1/script.js
@@ -0,0 +1,14 @@
+// JavaScript Sample
+
+//引数を用いた背景色の変更
+  function MyFunction05( color ){
+    const obj = document.body;
+    obj.style.backgroundColor = color;
+  }
+
+  
+// 背景色の変更
+  function MyFunction06(){
+    const obj = document.body;
+    obj.style.backgroundColor = randomColor();
+  }
